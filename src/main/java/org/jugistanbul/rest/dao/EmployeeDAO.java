ccd258275c67766911e566a9b9c92236e88ccba6
AltugAltintas <altuga@gmail.com>
2022-02-14 12:39:35 +0300

    src and pom are added

---
+++ src/main/java/org/jugistanbul/rest/dao/EmployeeDAO.java
@@ -0,0 +1,27 @@
+package org.jugistanbul.rest.dao;
+
+import org.jugistanbul.rest.model.Employee;
+import org.jugistanbul.rest.model.Employees;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class EmployeeDAO 
+{
+    private static Employees list = new Employees();
+    
+    static 
+    {
+        list.getEmployeeList().add(new Employee(1, "Lokesh", "Gupta", "howtodoinjava@gmail.com"));
+        list.getEmployeeList().add(new Employee(2, "Alex", "Kolenchiskey", "abc@gmail.com"));
+        list.getEmployeeList().add(new Employee(3, "David", "Kameron", "titanic@gmail.com"));
+    }
+    
+    public Employees getAllEmployees() 
+    {
+        return list;
+    }
+    
+    public void addEmployee(Employee employee) {
+        list.getEmployeeList().add(employee);
+    }
+}
